78bdba37286f414d5ba8fdbc51cf04a4aa44d215
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-03 04:16:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
+  if array.length < 1
+    array[-1].insert(0)
+  elsif array.length > 1
     array[-1].insert(0, "and ")
   end
   array.join(", ")
